e0304b3ac7774fc9858cff8fe269a4cb9ffbefbf
Nicolas Jacobsohn <nicolasjacobsohn@Nicolass-MacBook-Pro.local>
2019-08-28 15:53:36 -0600

    reorganized some code, looks nicer now

--- src/train_on_emulator.py
+++ src/train_on_emulator.py
@@ -23,6 +23,21 @@ def random_actions(env, print_steps=False, n_steps=1000):
             obs = env.reset()
     env.close()
 
+def make_environment(args):
+    """
+    Takes in parsed arg dictionary and returns functioning retro environment
+    """
+
+    env = retro.make(
+        game=args.game,
+        info=args.variables, #these are the variables I tracked down as well as their location in memory
+        obs_type=retro.Observations(args.observations), #0 for CNN image observation, 1 for NN numerical observation
+        state=retro.State.DEFAULT, #this isn't necessary but I'm keeping it for potential future customization
+        scenario=args.scenario,
+        record=args.record)
+    
+    return MarioDiscretizer(env) #wraps env to only allow hand chosen inputs and input combos
+
 
 if __name__ == "__main__":
 
@@ -31,59 +46,11 @@ if __name__ == "__main__":
     parser.add_argument("-s", '--scenario', default="scenarios/scenario.json", help="Try out a custom scenario")
     parser.add_argument("-o", "--observations", default=0, help="Either 0 or 1, 0 for screen observation, 1 for numerical observation", type=int)
     parser.add_argument("-r", "--record", default="learning_movies/", help="Choose a directory to record the training session to")
+    parser.add_argument("-v", "--variables", default="variables/powerup_relativeY_midway_positions.json", help="Path to reward variable json")
     args = parser.parse_args()
 
+    env = make_environment(args)
 
-    observation_type = retro.Observations(args.observations) #0 for CNN image observation, 1 for NN numerical observation
+    
 
-    reset_state = retro.State.DEFAULT #this isn't necessary but I'm keeping it for potential future customization
-
-    reward_variables = "variables/powerup_relativeY_midway_positions.json" #these are the variables I tracked down
-                                                                           #as well as their location in memory
-
-    env = retro.make(
-        game=args.game,
-        info=reward_variables, 
-        obs_type=observation_type, 
-        state=reset_state,
-        scenario=args.scenario,
-        record=args.record)
-
-    env = MarioDiscretizer(env) #wraps env to only allow hand chosen inputs and input combos
-
-    #random_actions(env, print_steps=True, n_steps=1000)
-
-
-
-#___________________________________ USEFUL INFORMATION ___________________________________#
-"""
-BUTTON INPUTS:
-['B', 'Y', 'SELECT', 'START', 'UP', 'DOWN', 'LEFT', 'RIGHT', 'A', 'X', 'L', 'R']
-
-BUTTON COMBOS:
-[[0, 16, 32],
- [0, 64, 128],
- [0, 1, 2, 3, 256, 257, 259, 512, 514, 515, 768, 770, 771],
- [0, 1024, 2048, 3072]]
-
-
-{
-    'B' : 1, 
-    'Y' : 2,
-    ['B', 'Y'] : 3,
-    'SELECT' : , 
-    'START' : , 
-    'UP' : , 
-    'DOWN' : , 
-    'LEFT' : , 
-    'RIGHT' : , 
-    'A' : , 
-    'X' : , 
-    'L' : , 
-    'R' : }
-
-
-
-
-
-"""
+    random_actions(env, print_steps=True, n_steps=1000)
