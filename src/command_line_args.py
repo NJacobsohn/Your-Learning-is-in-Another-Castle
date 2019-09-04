33affce8c4b8b5ecb6c03fce0bbb3fd3be2ca297
Nicolas Jacobsohn <nicolasjacobsohn@Nicolass-MacBook-Pro.local>
2019-09-04 14:33:27 -0600

    MORE script cleaning

--- src/command_line_args.py
+++ src/command_line_args.py
@@ -1,6 +1,13 @@
 import argparse
+from nn_mario_player import NNPlayer
+from cnn_mario_player import CNNPlayer
+from brute_mario_player import BrutePlayer
+from random_mario_player import RandomPlayer
 
 def create_parser():
+    """
+    Creates parser. This is over here because all the text looked messy in the train_on_emulator script
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--project", default="my_project",
         help="""Define the name of your project here. 
@@ -12,7 +19,7 @@ def create_parser():
     parser.add_argument("-o", "--observations", default=0, help="Either 0 or 1, 0 for screen observation, 1 for numerical observation", type=int)
     parser.add_argument("-r", "--record", default="learning_movies/", help="Choose a directory to record the training session to", type=str)
     parser.add_argument("-v", "--variables", default="variables/data.json", help="Path to reward variable json", type=str)
-    parser.add_argument("-a", "--algorithm", default="brute", 
+    parser.add_argument("-a", "--algorithm", default="random", 
         help="""Pick name of algorithm to run. 
                 Current options are:
                     Brute
@@ -23,9 +30,11 @@ def create_parser():
                 """)
     parser.add_argument("-m", "--model", default=None, 
         help="""Pick type of model to run with chosen algorithm.
-                This argument is only necessary when PPO1/2 is chosen for algorithm
+                This argument is only necessary (currently) when PPO is chosen for algorithm.
+
+                This argument does literally nothing if PPO isn't specified. 
+
                 Current options are:
-                    None
                     CNN *non-functioning model
                     NN *non-functioning model
 
@@ -35,3 +44,47 @@ def create_parser():
                 """)
                 
     return parser
+
+
+def choose_algorithm(args):
+        """
+        This is a (relatively) pythonic implementation of a switch statement.
+
+        It's technically a nested switch statement with the PPO option
+        Formatting it like this makes adding more models/algorithms very easy
+        """
+
+        switch_dict = {
+        "brute" : brute_alg,
+        "random" : random_alg,
+        "ppo" : {
+            "cnn" : cnn_model,
+            "nn" : nn_model
+            }
+        }
+
+        try:
+            model = args.model.lower()
+        except:
+            print("No model was chosem")
+
+        algorithm = args.algorithm.lower()
+
+        if algorithm == "ppo":
+            func = switch_dict.get(algorithm).get(model, "No model found for PPO") #nested .get for ppo dictionary
+        else:
+            func = switch_dict.get(algorithm, "No algorithm of type: {0} found".format(algorithm)) 
+            
+        return func(args)
+
+def brute_alg(args):
+    return BrutePlayer(args.project, args.game, args.scenario, args.variables, args.observations, args.record)
+
+def random_alg(args):
+    return RandomPlayer(args.project, args.game, args.scenario, args.variables, args.observations, args.record)
+
+def cnn_model(args):
+    return CNNPlayer(args.project, args.game, args.scenario, args.variables, args.observations, args.record)
+
+def nn_model(args):
+    return NNPlayer(args.project, args.game, args.scenario, args.variables, args.observations, args.record)
--- src/train_on_emulator.py
+++ src/train_on_emulator.py
@@ -1,29 +1,9 @@
 import argparse
-from nn_mario_player import NNPlayer
-from cnn_mario_player import CNNPlayer
-from brute_mario_player import BrutePlayer
-from command_line_args import create_parser
-from random_mario_player import RandomPlayer
-
-def choose_algorithm(args):
-    if args.algorithm.lower() == "brute":
-        return BrutePlayer(args.project, args.game, args.scenario, args.variables, args.observations, args.record)
-
-    elif args.algorithm.lower() == "random":
-        return RandomPlayer(args.project, args.game, args.scenario, args.variables, args.observations, args.record)
-
-    elif args.algorithm.lower() == "ppo":
-        try:
-            model_type = args.model.lower()
-        except:
-            return "You specified PPO but didn't specify a model to optimize"
-
-        if model_type == "cnn": 
-            return CNNPlayer(args.project, args.game, args.scenario, args.variables, args.observations, args.record)
-            
-        elif model_type == "nn":
-            return NNPlayer(args.project, args.game, args.scenario, args.variables, args.observations, args.record)
-
+#from nn_mario_player import NNPlayer
+#from cnn_mario_player import CNNPlayer
+#from brute_mario_player import BrutePlayer
+#from random_mario_player import RandomPlayer
+from command_line_args import create_parser, choose_algorithm
 
 
 if __name__ == "__main__":
@@ -33,5 +13,4 @@ if __name__ == "__main__":
 
     alg = choose_algorithm(args)
     
-
     alg.run()
